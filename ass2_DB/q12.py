b803e581b9933ac3078d124d6d0c7ac79db77e77
dor-darmon <161502302+dor-darmon@users.noreply.github.com>
2024-12-14 13:57:42 +0200

    Update q12.py

--- ass2_DB/q12.py
+++ ass2_DB/q12.py
@@ -1,32 +1,32 @@
 import mysql.connector
-if __name__ == '__main__':
-        # Connect to the database
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="root",
-            database="biu_shoes",
-            port="3307",
-        )
-        cursor = mydb.cursor()
 
-        # Select the shoe name from the 'shoe' table
-        # The 'shoe' table is aliased as 's'
-        # Perform a LEFT JOIN with the 'order_shoe' table (aliased as 'os')
-        # Join the tables where shoe_id matches in both tables
-        # Filter out the shoes that have not been ordered (no matching shoe_id in 'order_shoe')
-        # Display unsold shoes.
-        #
-        query = """
-        SELECT GROUP_CONCAT(s.shoe_name SEPARATOR ', ') AS unsold_shoes
-        FROM shoe AS s
-        LEFT JOIN order_shoe AS os
-        ON s.shoe_id = os.shoe_id
-        WHERE os.shoe_id IS NULL;
-        """
-        cursor.execute(query)
-        unsold_shoes = cursor.fetchone()
-        if unsold_shoes and unsold_shoes[0]:
-            print(f"{unsold_shoes[0]}")
-        cursor.close()
-        mydb.close()
+if __name__ == '__main__':
+    # Connect to the database
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="biu_shoes",
+        port="3307",
+    )
+    
+    cursor = mydb.cursor()
+
+    # Select the shoe name from the 'shoe' table
+    # The 'shoe' table is aliased as 's'
+    # Perform a LEFT JOIN with the 'order_shoe' table (aliased as 'os')
+    # Join the tables where shoe_id matches in both tables
+    # Filter out the shoes that have not been ordered (no matching shoe_id in 'order_shoe')
+    # Display unsold shoes.
+    query = """
+    SELECT s.shoe_name
+    FROM shoe AS s
+    LEFT JOIN order_shoe AS os
+    ON s.shoe_id = os.shoe_id
+    WHERE os.shoe_id IS NULL;
+    """
+    cursor.execute(query)
+    unsold_shoes = cursor.fetchall()
+    print(', '.join(str(row[0]) for row in unsold_shoes))
+    cursor.close()
+    mydb.close()
